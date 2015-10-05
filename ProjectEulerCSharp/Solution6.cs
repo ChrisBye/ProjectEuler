de01bd4ad91865b718c623e5367a9df852d61b6a
Chris Bye <chriswbye@gmail.com>
2015-10-04 22:47:24 -0400

    solved 6

---
+++ ProjectEulerCSharp/Solution6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using ProjectEulerCore.Infrastructure;
+using System.Linq;
+
+namespace ProjectEulerCSharp
+{
+    [Problem(6, "Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.")]
+    public class Solution6
+    {
+        public object Solve()
+        {
+            var squareOfSum = Math.Pow(Enumerable.Range(1, 100).Sum(),2);
+            var sumOfSquares = Enumerable.Range(1, 100).Select(i => Math.Pow(i,2)).Sum();
+            var solution = squareOfSum - sumOfSquares;
+            return solution;
+        }
+    }
+}
