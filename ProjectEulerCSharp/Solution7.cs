92a988caf39b954bd8fc6fab657b518361a5ae5e
Chris Bye <chriswbye@gmail.com>
2015-10-04 22:49:14 -0400

    solved 7

---
+++ ProjectEulerCSharp/Solution7.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ProjectEulerCore.Infrastructure;
+using System.Linq;
+
+namespace ProjectEulerCSharp
+{
+    [Problem(7, "What is the 10 001st prime number?")]
+    public class Solution7
+    {
+        public object Solve()
+        {
+            var solution = Sequences.Primes().ElementAt(10000);
+            return solution;
+        }
+    }
+}
