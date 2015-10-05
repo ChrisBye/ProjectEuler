1f4afaf714aa2708d94892b58912eda5e22c5a7c
Chris Bye <chriswbye@gmail.com>
2015-10-05 02:10:42 -0400

    solved 16

---
+++ ProjectEulerCSharp/Solution016.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using ProjectEulerCore.Infrastructure;
+using System.Linq;
+using System.Numerics;
+
+namespace ProjectEulerCSharp
+{
+    [Problem(16, "Power digit sum")]
+    public class Solution016
+    {
+        public object Solve()
+        {
+            BigInteger number = BigInteger.Pow(2, 1000);
+            var total = 0;
+            while (number > 0)
+            {
+                total += (int)(number % 10);
+                number = number/10;
+            }
+            
+            return total;
+        }
+    }
+}
